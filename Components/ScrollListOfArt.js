dc983507bf63eaccc7e97b54cf1ebc91aebe0fc8
ankita6241 <121148600+ankita6241@users.noreply.github.com>
2022-12-21 15:05:22 +0530

    Delete ScrollListOfArt.js

--- Components/ScrollListOfArt.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const ScrollListOfArt =(props) =>{
-    return(
-        <div style={{overflowY:"scroll",height:'70vh'}}>{props.children}</div>
-    );
-}
-
-export default ScrollListOfArt;
