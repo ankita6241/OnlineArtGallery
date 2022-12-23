eab2687e6f3ab7c01128911bcca110294c7e11df
ankita6241 <121148600+ankita6241@users.noreply.github.com>
2022-12-23 20:26:56 +0530

    Updated

--- App.js
+++ App.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import SignUp from './Components/SignUp';
+import SignIn from './Components/SignIn';
 import Header from './Navigation/Header';
-import Home from './Components/Home';
-import About from './Components/About';
-import AllProducts from './Components/AllProducts';
-import CustomerLogin from './Components/CustomerLogin';
-import AdminLogin from './Components/AdminLogin';
-import Registeration from './Components/Registeration';
-import Contact from './Components/Contact';
-import FeedBack from './Components/FeedBack';
+import Error from './Components/Error';
+import CustomerHome from './Components/CustomerHome';
+//import About from './Components/About';
+//import AllProducts from './Components/AllProducts';
+//import CustomerLogin from './Components/CustomerLogin';
+//import AdminLogin from './Components/AdminLogin';
+//import Registration from './Components/Registration';
+//import Contact from './Components/Contact';
+//import FeedBack from './Components/FeedBack';
 import {Route,Routes} from 'react-router-dom';
+import BgImage from './Images/BgImage.jpg';
+function App(){
 
-
-function App() {
-  return (
-    <div>
-      <Header/>
-       <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route path="/About" element={<About/>}/>
-        <Route path="/AllProducts" element={<AllProducts/>}/>
-        <Route path="/AdminLogin" element={<AdminLogin/>}/>
-        <Route path="/CustomerLogin" element={<CustomerLogin/>}/>
-        <Route path="/Registration" element={<Registeration/>}/>
-        <Route path="/Contact" element={<Contact/>}/>
-        <Route path="/Feedback" element={<FeedBack/>}/>
-       </Routes>
-    </div>
-  );
-}
+     return(
+      <>
+      <div>
+        <Routes>
+          <Route path='/' element={<SignUp/>}/>
+          <Route path='/SignIn' element={<SignIn/>}/>
+          <Route path='/Error' element={<Error/>}/>
+          <Route path='/Home' element={<CustomerHome/>}/>
+      </Routes>
+      </div>
+      </>
+    )
+     
+  }
 
 export default App;
+
